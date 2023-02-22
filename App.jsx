4e0312ac86cc6d02da8b92e813085497fd114bc4
ramusini <97661414+ramusini@users.noreply.github.com>
2023-02-22 23:51:54 +0900

    [add] component Hello.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world</Text>
-      <StatusBar style="auto" />
+      <Hello/>
     </View>
   );
 }
